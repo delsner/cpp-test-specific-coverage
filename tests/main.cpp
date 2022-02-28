53efa6d245d67d1b1a3d9a8518e6669e9c4ce03e
Daniel Elsner <dvelsner@gmail.com>
2022-02-28 21:20:35 +0100

    Move test listener into separate static library

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,19 @@
 #include <gtest/gtest.h>
+
+#ifdef TEST_LISTENER
 #include "cov_listener.h"
+#endif
 
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+#ifdef TEST_LISTENER
     // Gets hold of the event listener list.
     ::testing::TestEventListeners &listeners =
             ::testing::UnitTest::GetInstance()->listeners();
     // Adds a listener to the end.
     // googletest takes the ownership.
     listeners.Append(new CoverageEventListener());
+#endif
     return RUN_ALL_TESTS();
 }
